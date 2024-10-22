01fb7cccc298363e6ce50fc29d6942492713767e
Воробьева Полина <pvorobyova@kamaz.digital>
2024-10-22 09:45:54 +0300

    move axis almost correct

--- src/main.js
+++ src/main.js
@@ -28,10 +28,10 @@ const drawArc = () => {
   //createArc(center,1002,Cesium.Math.toRadians(0),Cesium.Math.toRadians(90.0),Cesium.Color.BLUEVIOLET)
 };
 const names = new Set(["X", "Y", "Z", "R", "SPHERE"]);
-console.log(names, "names");
+
 function drawSphere() {
-  const radius = 1000;
-  const len = 1100;
+  const radius = 100;
+  const len = 120;
   // Создаем сферу
   const sphere = viewer.entities.add({
     position: center,
@@ -94,7 +94,7 @@ function drawSphere() {
     const normalizedAxis = normalizeVector(axisVector); // Нормализация с проверкой
     const offset = Cesium.Cartesian3.multiplyByScalar(
       normalizedAxis,
-      100,
+      len / 10,
       new Cesium.Cartesian3(),
     );
     const startPoint = Cesium.Cartesian3.add(
@@ -110,7 +110,7 @@ function drawSphere() {
           startPoint,
           Cesium.Cartesian3.add(position, axisVector, new Cesium.Cartesian3()),
         ],
-        width: 5.0,
+        width: 10.0,
         material: Cesium.Color[color],
         clampToGround: false,
       },
@@ -153,42 +153,6 @@ const button = document.getElementById("draw");
 button.addEventListener("click", drawArc);
 let isDragging = false;
 
-function drawArrow() {
-  const p = {
-    name: "ARROW UP",
-    position: Cesium.Cartesian3.fromDegrees(lon, lat, HEIGHT),
-    cylinder: {
-      length: 400.0,
-      topRadius: 40.0,
-      bottomRadius: 40.0,
-      material: Cesium.Color.YELLOW,
-    },
-  };
-  const greenCylinder = viewer.entities.add(p);
-  currentPrimitives.add(p);
-  //arcPrimitive.on(Cesium.ScreenSpaceEventType.LEFT_CLICK, function(click) {
-  //     // Обработка клика по дуге
-  //     console.log('Вы кликнули по дуге');
-  // });
-  const y = {
-    name: "Red cone",
-    position: Cesium.Cartesian3.fromDegrees(lon, lat, 2050.0),
-    properties: {
-      id: "GRGRGRGR",
-    },
-    cylinder: {
-      length: 50.0,
-      topRadius: 0.0,
-      bottomRadius: 80.0,
-      material: Cesium.Color.RED,
-    },
-  };
-  const redCone = viewer.entities.add(y);
-  currentPrimitives.add(y);
-
-  viewer.zoomTo(viewer.entities);
-  clickHandler();
-}
 function cameraBlockBehavior(isNormalBehavior) {
   viewer.scene.screenSpaceCameraController.enableRotate = isNormalBehavior;
   viewer.scene.screenSpaceCameraController.enableTranslate = isNormalBehavior;
@@ -206,127 +170,89 @@ function listenMoveStop(click) {
 let t = false;
 
 function listenMouseMove(click) {
-  console.log("move", click.endPosition, click.startPosition);
+  const inverse = viewer.camera.inverseViewMatrix;
   if (click.startPosition.y === click.endPosition.y) return;
-  const direction = click.startPosition.y < click.endPosition.y ? "down" : "up";
-  console.log(
-    direction,
-    ">>",
-    click.startPosition.x,
-    "-->",
-    click.endPosition.x,
-  );
-  //const difference= click.endPosition.x-click.startPosition.x
-  //   console.log(difference,'diff')
 
-  // Преобразуем экранные координаты в декартовы для стартовой точки
-  const cartesianStart = viewer.camera.pickEllipsoid(
-    click.startPosition,
-    viewer.scene.globe.ellipsoid,
+  const cartesianStart = Cesium.Matrix4.multiplyByPoint(
+    inverse,
+    new Cesium.Cartesian3(click.startPosition.x, -click.startPosition.y, 0), //у экрана У направлена вниз поэтому -
+    new Cesium.Cartesian3(),
   );
+
   // Преобразуем декартовы координаты в геодезические
   const cartographicStart = Cesium.Cartographic.fromCartesian(cartesianStart);
-  const longitudeStart = Cesium.Math.toDegrees(cartographicStart.longitude); //*(direction==='up'?-1:1);
-  const latitudeStart = Cesium.Math.toDegrees(cartographicStart.latitude);
-  //const startCartesian=new Cesium.Cartesian3(longitude,latitude)
+  //const longitudeStart = Cesium.Math.toDegrees(cartographicStart.longitude); //*(direction==='up'?-1:1);
+  //const latitudeStart = Cesium.Math.toDegrees(cartographicStart.latitude);
 
-  // Преобразуем экранные координаты в декартовы для конечной
-  const cartesian = viewer.camera.pickEllipsoid(
-    click.endPosition,
-    viewer.scene.globe.ellipsoid,
+  const cartesian = Cesium.Matrix4.multiplyByPoint(
+    inverse,
+    new Cesium.Cartesian3(click.endPosition.x, -click.endPosition.y, 0),
+    new Cesium.Cartesian3(),
   );
   // Преобразуем декартовы координаты в геодезические
   const cartographic = Cesium.Cartographic.fromCartesian(cartesian);
-  const longitude = Cesium.Math.toDegrees(cartographic.longitude); //*(direction==='up'?-1:1);
-  const latitude = Cesium.Math.toDegrees(cartographic.latitude);
-  console.log("LLLLLLLLLLLLL", longitude);
-  console.log("LAT", latitude);
+  //const longitude = Cesium.Math.toDegrees(cartographic.longitude); //*(direction==='up'?-1:1);
+  //const latitude = Cesium.Math.toDegrees(cartographic.latitude);
 
   const sub = Cesium.Cartesian3.subtract(
     cartesian,
     cartesianStart,
     new Cesium.Cartesian3(),
   );
-  console.log("subs-", sub);
   const axisVector = primitive.id?.properties.axis.getValue();
-  console.log("axisVector", axisVector);
-  const magnitude = Cesium.Cartesian3.magnitude(sub);
-  console.log("magnitude", magnitude);
+  //const magnitude = Cesium.Cartesian3.magnitude(sub);
   const axisvectorMagnitude = Cesium.Cartesian3.magnitude(axisVector);
   const name = primitive.id?.properties.name.getValue();
   let dot = Cesium.Cartesian3.dot(sub, axisVector);
-  dot = name === "Z" ? 0.001 : dot / (magnitude * axisvectorMagnitude);
-  console.log("dot", dot);
+
+  dot /= axisvectorMagnitude * axisvectorMagnitude; //высчитываем коэф шага движения !!!!
 
   const translation = Cesium.Cartesian3.multiplyByScalar(
     axisVector,
     dot,
     new Cesium.Cartesian3(),
   );
+  primitive.primitive.modelMatrix.ge;
 
-  const translateOld = new Cesium.Cartesian3(
-    primitive.primitive.modelMatrix[12],
-    primitive.primitive.modelMatrix[13],
-    primitive.primitive.modelMatrix[14],
+  const translateOld = Cesium.Matrix4.getTranslation(
+    primitive.primitive.modelMatrix,
+    new Cesium.Cartesian3(),
   );
 
   const hh = Cesium.Matrix4.fromTranslation(
     Cesium.Cartesian3.add(translation, translateOld, new Cesium.Cartesian3()),
   );
 
-  // const hh = Cesium.Matrix4.fromTranslation(translation);
-  //new Cesium.Cartesian3(
-  /*    name === "X" ? dot : 0,
-      name === "Y" ? dot : 0,
-      name === "Z" ? dot : 0,
-    ), */
-  //вычислить и для старт и конеч lat long движ
-  //cart = cartesian3
-  //substruct (end-start)
-  //Cartesian3.dot( substruct (end-start),'ось- вектор оси' )= float если положит или отрицательн
-  //в матрицу трансейт передать по нужной оси .dot вместо logn
-  console.log(
-    "primitive.primitive.modelMatrix",
-    primitive.primitive.modelMatrix,
-  );
   const newModelMatrix = Cesium.Matrix4.multiply(
     primitive.primitive.modelMatrix,
     hh,
     new Cesium.Matrix4(),
   );
-  console.log("newModelMatrix", newModelMatrix);
-  //console.log(viewer.scene,'PR')
 
-  ///data.forEach(el=>{
-  // console.log(el?.position)
-  // el.position=new Cesium.Cartesian3(0,0,0)
-  //})
   primitive.primitive.modelMatrix = hh; //newModelMatrix;
   if (isDragging) {
-    // Здесь ваш код для перемещения объекта или выполнения других действий
-    // Отключаем стандартное поведение камеры
     cameraBlockBehavior(false);
   }
-  //sconsole.log('Мышь движется');
-  // handler.removeInputAction(Cesium.ScreenSpaceEventType.LEFT_DOWN);
-  // Здесь ваш код, который будет выполняться при нажатии на левую кнопку мыши
-
   handler.setInputAction(listenMoveStop, Cesium.ScreenSpaceEventType.LEFT_UP);
 }
 
 function debounce(func, wait) {
   let timeout;
-
+  if (isDragging) {
+    // cameraBlockBehavior(false);
+  }
   return function () {
     const context = this;
+
     const args = arguments;
     clearTimeout(timeout);
     timeout = setTimeout(() => {
+      //isDragging = false;
       func.apply(context, args);
     }, wait);
   };
 }
-const debouncedMouseMove = debounce(listenMouseMove, 100); // Вызываем функцию каждые 100 мс
+const debouncedMouseMove = debounce(listenMouseMove, 50); // Вызываем функцию каждые 100 мс
 
 function listenMouseDown(click) {
   console.log("down listen");
@@ -341,19 +267,16 @@ function listenMouseDown(click) {
         el.id?.properties?.name.toString() === "Y" ||
         el.id?.properties?.name.toString() === "X",
     );
-  //console.log("TTTTTTT",viewer.scene.drillPick(click.position).map(yt=>yt.id?.name))
   if (t) {
     primitive = t;
   } else {
     cameraBlockBehavior(true);
     return;
   }
-  // viewer.scene.drillPick(click.position).forEach(yt=>console.log("$$$$$$",yt.id?.name))
-  //if(!viewer.scene.pick(click.position).primitive)return
   isDragging = true;
-  // Здесь ваш код, который будет выполняться при нажатии на левую кнопку мыши
   handler.setInputAction(
     listenMouseMove,
+    //debouncedMouseMove,
     Cesium.ScreenSpaceEventType.MOUSE_MOVE,
   );
 }
